f77f8137ec7bebf77a81acd35a5c3e30205cc0e9
dfqxyfy <crazyanyone@163.com>
2016-11-07 00:36:35 +0800

    星座头像

---
+++ app/src/main/java/com/example/ccs/entity/Stars.java
@@ -0,0 +1,79 @@
+package com.example.ccs.entity;
+
+import com.example.ccs.myandriod.R;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by ccs on 2016/11/6.
+ */
+
+public enum Stars {
+
+    //水瓶座，双鱼座，白羊座，金牛座，双子座，巨蟹座，狮子座，处女座，天秤座，天蝎座，射手座，摩羯座
+    SHUIPING_STAR(1,"1.20","2.18",R.mipmap.stars_0002),
+    SHUANGYU_STAR(2,"2.19","3.20",R.mipmap.stars_0003),
+    BAIYANG_STAR(3,"3.21","4.19",R.mipmap.stars_0004),
+    JINNIU_STAR(4,"4.20","5.20",R.mipmap.stars_0005),
+    SHUANGZI_STAR(5,"5.21","6.21",R.mipmap.stars_0006),
+    JUXIE_STAR(6,"6.22","7.22",R.mipmap.stars_0007),
+    SHIZI_STAR(7,"7.23","8.22",R.mipmap.stars_0008),
+    CHUNV_STAR(8,"8.23","9.22",R.mipmap.stars_0009),
+    TIANPING_STAR(9,"9.23","10.23",R.mipmap.stars_0010),
+    TIANXIE_STAR(10,"10.24","11.22",R.mipmap.stars_0011),
+    SHESHOU_STAR(11,"11.23","12.21",R.mipmap.stars_0012),
+    MOXIE_STAR(12,"12.21","1.19",R.mipmap.stars_0001),
+    ;
+
+    private Integer num;
+    private Integer pic;
+    private String starDate;
+    private String endDate;
+
+    private Stars(Integer num,String starDate,String endDate,Integer pic){
+        this.num = num;
+        this.pic = pic;
+        this.starDate = starDate;
+        this.endDate = endDate;
+    }
+
+    public Integer getNum(Integer num){
+        return num;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public Integer getPic() {
+        return pic;
+    }
+
+    public String getStarDate() {
+        return starDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public Stars getStarByDate(Date d){
+        SimpleDateFormat sdf = new SimpleDateFormat("M.dd");
+        Date start = null;
+        Date end = null;
+        try {
+            start = sdf.parse(this.starDate);
+            end = sdf.parse(this.endDate);
+        } catch (ParseException e) {
+        }
+        //Calendar.get(Calendar.MONTH);
+        Integer month = d.getMonth();
+        Integer day = d.getDay();
+
+        return Stars.BAIYANG_STAR;
+
+    }
+}
