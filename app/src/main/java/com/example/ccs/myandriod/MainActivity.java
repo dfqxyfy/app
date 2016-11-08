bfe4b462b718d16c286ce2c6ec1772099ee88991
chaichuanshi <chaichuanshi@le.com>
2016-11-08 10:40:42 +0800

    点击返回

--- app/src/main/java/com/example/ccs/myandriod/MainActivity.java
+++ app/src/main/java/com/example/ccs/myandriod/MainActivity.java
@@ -25,6 +25,7 @@ import android.widget.TextView;
 import com.ccs.compute.CommStar;
 import com.ccs.compute.LauarTool;
 import com.ccs.compute.ZodiacTool;
+import com.example.ccs.entity.Stars;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -38,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,71 +58,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //final EditText lauarText = (EditText) findViewById(R.id.lauarText);
-
         final Button btn = (Button) findViewById(R.id.button);
-        final Button detaiBtn = (Button) findViewById(R.id.detail);
-        final String[] stars = {"双鱼座","金牛座"};
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                detaiBtn.setBackgroundResource(choosePic(CommStar.star));
-
-                //AlertDialog ad = new AlertDialog.Builder(MainActivity.this);
-//                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this).setTitle("选择星座");
-//
-//                builder.setItems(stars, new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//
-//                    }
-//                });
-//                builder.show();
-                //Intent intent = new Intent(getMain(),SelStarActivity.class);
+                btn.setText("选星座");
                 Intent intent = new Intent(MainActivity.this,SelStarActivity.class);
-                Log.v("v debug","test");
-                Log.e("e debug","test");
-                Log.d("d debug","test");
                 try {
-                    //startService(intent);
-                    //startActivity(intent);
                     startActivityForResult(intent,0);
                 }catch (Throwable e){
                     Log.e("ttttttteeeeeeee debug","test",e);
                 }
-//                if(true)
-//                    return;
-//                startService(intent);
-//
-//                try{
-//                    Thread.sleep(1000);
-//                }catch (Exception e){
-//
-//                }
-//
-//                stopService(intent);
-            }
-        });
 
-
-
-        detaiBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                final GridLayout layout = (GridLayout)findViewById(R.id.ccsgridLayout);
-                layout.removeAllViews();
-                //final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.detailText);
-                for(int i = 0 ;i< CommStar.getLsDetail().size();i++){
-                    StarDescText sdt = new StarDescText(getMain(),CommStar.getLsDetail().get(i).getType(),CommStar.getLsDetail().get(i).getDes());
-                    layout.addView(sdt);
-
-                }
-                layout.refreshDrawableState();
-                final TextView et = (TextView)findViewById(R.id.starDetail);
-                et.setSingleLine(false);
-                et.setText("\t\t\t\t白羊座有一种让人看见就觉得开心的感觉，因为总是看起来都是那么地热情、阳光、乐观、坚强，对朋友也慷概大方，性格直来直往，就是有点小脾气。白羊男有大男人主义的性格，而白羊女就是女汉子的形象。");
             }
         });
 
@@ -128,7 +80,12 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        Bundle bundle = data.getExtras();
+        String str = bundle.getString("str");
+        final Button detaiBtn = (Button) findViewById(R.id.detail);
+        detaiBtn.setText("");
+        Log.e("eee",str);
+        detaiBtn.setBackgroundResource(Stars.getStarByNum(Integer.valueOf(str)).getPic());
     }
 
     private final MainActivity getMain(){
@@ -137,19 +94,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
         }
@@ -161,8 +113,6 @@ public class MainActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
         client.connect();
         Action viewAction = Action.newAction(
                 Action.TYPE_VIEW, // TODO: choose an action type.
@@ -197,35 +147,4 @@ public class MainActivity extends AppCompatActivity {
         client.disconnect();
     }
 
-    private int choosePic(Integer star) {
-        switch (star) {
-            case 0:
-                return R.mipmap.stars_0001;
-            case 1:
-                return R.mipmap.stars_0002;
-            case 2:
-                return R.mipmap.stars_0003;
-            case 3:
-                return R.mipmap.stars_0004;
-            case 4:
-                return R.mipmap.stars_0005;
-            case 5:
-                return R.mipmap.stars_0006;
-            case 6:
-                return R.mipmap.stars_0007;
-            case 7:
-                return R.mipmap.stars_0008;
-            case 8:
-                return R.mipmap.stars_0009;
-            case 9:
-                return R.mipmap.stars_0010;
-            case 10:
-                return R.mipmap.stars_0011;
-            case 11:
-                return R.mipmap.stars_0012;
-            case 12:
-                return R.mipmap.stars_0001;
-        }
-        return 0;
-    }
 }
