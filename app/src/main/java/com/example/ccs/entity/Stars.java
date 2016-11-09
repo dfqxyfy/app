9451971e4f746fd107155a76cfd9084b579bd560
dfqxyfy <crazyanyone@163.com>
2016-11-10 00:30:31 +0800

    星座头像

--- app/src/main/java/com/example/ccs/entity/Stars.java
+++ app/src/main/java/com/example/ccs/entity/Stars.java
@@ -14,18 +14,18 @@ import java.util.Date;
 public enum Stars {
 
     //水瓶座，双鱼座，白羊座，金牛座，双子座，巨蟹座，狮子座，处女座，天秤座，天蝎座，射手座，摩羯座
-    SHUIPING_STAR(1,"1.20","2.18",R.mipmap.stars_0002,"水瓶座"),
-    SHUANGYU_STAR(2,"2.19","3.20",R.mipmap.stars_0003,"双鱼座"),
-    BAIYANG_STAR(3,"3.21","4.19",R.mipmap.stars_0004,"白羊座"),
-    JINNIU_STAR(4,"4.20","5.20",R.mipmap.stars_0005,"金牛座"),
-    SHUANGZI_STAR(5,"5.21","6.21",R.mipmap.stars_0006,"双子座"),
-    JUXIE_STAR(6,"6.22","7.22",R.mipmap.stars_0007,"巨蟹座"),
-    SHIZI_STAR(7,"7.23","8.22",R.mipmap.stars_0008,"狮子座"),
-    CHUNV_STAR(8,"8.23","9.22",R.mipmap.stars_0009,"处女座"),
-    TIANPING_STAR(9,"9.23","10.23",R.mipmap.stars_0010,"天秤座"),
-    TIANXIE_STAR(10,"10.24","11.22",R.mipmap.stars_0011,"天蝎座"),
-    SHESHOU_STAR(11,"11.23","12.21",R.mipmap.stars_0012,"射手座"),
-    MOXIE_STAR(12,"12.21","1.19",R.mipmap.stars_0001,"摩羯座"),
+    BAIYANG_STAR(1,"3.21","4.19",R.mipmap.stars_0004,"白羊座"),
+    JINNIU_STAR(2,"4.20","5.20",R.mipmap.stars_0005,"金牛座"),
+    SHUANGZI_STAR(3,"5.21","6.21",R.mipmap.stars_0006,"双子座"),
+    JUXIE_STAR(4,"6.22","7.22",R.mipmap.stars_0007,"巨蟹座"),
+    SHIZI_STAR(5,"7.23","8.22",R.mipmap.stars_0008,"狮子座"),
+    CHUNV_STAR(6,"8.23","9.22",R.mipmap.stars_0009,"处女座"),
+    TIANPING_STAR(7,"9.23","10.23",R.mipmap.stars_0010,"天秤座"),
+    TIANXIE_STAR(8,"10.24","11.22",R.mipmap.stars_0011,"天蝎座"),
+    SHESHOU_STAR(9,"11.23","12.21",R.mipmap.stars_0012,"射手座"),
+    MOXIE_STAR(10,"12.21","1.19",R.mipmap.stars_0001,"摩羯座"),
+    SHUIPING_STAR(11,"1.20","2.18",R.mipmap.stars_0002,"水瓶座"),
+    SHUANGYU_STAR(12,"2.19","3.20",R.mipmap.stars_0003,"双鱼座"),
     ;
 
     private Integer num;
